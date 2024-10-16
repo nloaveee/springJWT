a59d38bb70dc1595d7b6d16fc4d0bac1ea4d5e38
hyeonheee <nloavee@gmail.com>
2024-10-16 11:17:23 +0900

    세션 정보

--- src/main/java/study/springjwt/controller/MainController.java
+++ src/main/java/study/springjwt/controller/MainController.java
@@ -1,9 +1,15 @@
 package study.springjwt.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
@@ -12,8 +18,16 @@ public class MainController {
     @GetMapping("/")
     public String homeP() {
 
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
 
-        return "Main controller";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "Main controller" + username + role;
     }
 
 
