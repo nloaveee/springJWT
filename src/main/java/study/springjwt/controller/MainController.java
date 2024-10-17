69ec41382d72f662a3d645a8615dcc78b9c74cb4
hyeonheee <nloavee@gmail.com>
2024-10-17 14:58:00 +0900

    다중 토큰 발급 (Access,Refresh)

--- src/main/java/study/springjwt/controller/MainController.java
+++ src/main/java/study/springjwt/controller/MainController.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collection;
